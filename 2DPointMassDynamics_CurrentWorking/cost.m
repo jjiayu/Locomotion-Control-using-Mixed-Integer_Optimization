482a74907201054f07a9cdead8e50f3d81531e89
unknown <jjiayu889@gmail.com>
2019-01-08 19:44:41 +0000

    2D mass moving without kinematics and robotation constraints; Can be solved by either Knitro and Gurobi

--- 2DPointMassDynamics_CurrentWorking/cost.m
+++ 2DPointMassDynamics_CurrentWorking/cost.m
@@ -3,10 +3,6 @@ function cost = cost(vars, Q)
 %   vars: decision variable vector, vars should be a column vector
 %   Q: selection matrix for quadratic form
 cost = vars'*Q*vars;
-%cost = (vars(21)-10)^2+(vars(42)-0)^2 + vars'*Q*vars;
-%--------------------------------------------------------------------------
-%   Test Code
-%disp('cost is ')
-%disp(cost)
+
 end
 
