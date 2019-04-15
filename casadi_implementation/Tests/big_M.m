47ce04dc90bc348d7365465d8c0746b0daa0cb80
jiayu <jjiayu889@gmail.com>
2019-04-15 22:11:16 +0100

    chenck complementarity constriant in casadi using big-M

--- casadi_implementation/Tests/big_M.m
+++ casadi_implementation/Tests/big_M.m
@@ -16,12 +16,20 @@ import casadi.*
 % 
 M = 1e5;
 x = SX.sym('x');
-c = SX.sym('x');
+c = SX.sym('c');
 v = [x;c];
-f = x^2;
-g = x - c*M;
+f = x^2 + c^2;
+g = {};
+lbg = [];
+ubg = [];
+g = {g{:},x + M*c};
+lbg = [lbg;0];
+ubg = [ubg;inf];
+g = {g{:}, x - M*c};
+lbg = [lbg;-inf];
+ubg = [ubg;0];
 discrete = [0,1];
-nlp = struct('x', v, 'f', f, 'g', g);
+nlp = struct('x', v, 'f', f, 'g', vertcat(g{:}));
 
 % Create IPOPT solver object
 solver = nlpsol('solver', 'knitro', nlp,struct('discrete', discrete));
@@ -30,8 +38,8 @@ solver = nlpsol('solver', 'knitro', nlp,struct('discrete', discrete));
 res = solver('x0' , [2.5 1.0],... % solution guess
              'lbx', [-100,0],...           % lower bound on x
              'ubx', [100,1],...           % upper bound on x
-             'lbg',  -M + 1,...           % lower bound on g
-             'ubg',    1);            % upper bound on g
+             'lbg',  lbg,...           % lower bound on g
+             'ubg',  ubg);            % upper bound on g
  
 % Print the solution
 f_opt = full(res.f)          % >> 0
