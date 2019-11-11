e8a795d5574449d1c039c73bc9157d72c0401d89
jiayu <jjiayu889@gmail.com>
2019-11-11 10:51:22 +0000

    Big Mistake: Limb Kinematics Contraint, Velocity Constraint, the rotation matrix is defined by the one from robot frame to the world frame. But We should use the one from world frame to the robot frame. (the standard other than the invere/rotated one)

--- InitialGuess_DataProcessing/Extract_MINLP_Trajectories.m
+++ InitialGuess_DataProcessing/Extract_MINLP_Trajectories.m
@@ -42,15 +42,49 @@ for loopIdx = 1:length(StridePeriodVector)
                                     'FHx_result', 'FHy_result',...
                                     'PhaseSwitchingTime',... %Note, Phase Switching Time Start from a constant zero
                                     'CF_result',   'CH_result',...
+                                    'PFcenterX_result_world','PFcenterY_result_world',...
+                                    'PHcenterX_result_world','PHcenterY_result_world',...
                                     'result_cost');
     MINLP_OptResult.PhaseSwitchingTime = MINLP_OptResult.PhaseSwitchingTime(2:end);
+    %   Save Result labels
+    MINLP_OptResult.StridePeriod = StridePeriodVector(loopIdx);
+    MINLP_OptResult.Speed = SpeedVector(loopIdx);
+    MINLP_OptResult.MINLP_Result_FileName = InitialGuessFileName;
+    %Transform the world frame quantities into robot frame
+    %   Foot Step Locations
+    for knotIdx = 1:length(MINLP_OptResult.theta_result)
+        %Front Leg Position
+        FrontLeg_Position_Temp = [cos(MINLP_OptResult.theta_result(knotIdx)),sin(MINLP_OptResult.theta_result(knotIdx));-sin(MINLP_OptResult.theta_result(knotIdx)),cos(MINLP_OptResult.theta_result(knotIdx))]*([MINLP_OptResult.PFx_result(knotIdx);MINLP_OptResult.PFy_result(knotIdx)] - [MINLP_OptResult.x_result(knotIdx);MINLP_OptResult.y_result(knotIdx)]);
+        MINLP_OptResult.PFx_result_Robot(knotIdx) = FrontLeg_Position_Temp(1);
+        MINLP_OptResult.PFy_result_Robot(knotIdx) = FrontLeg_Position_Temp(2);
+        
+        %Hind Leg Position
+        HindLeg_Position_Temp = [cos(MINLP_OptResult.theta_result(knotIdx)),sin(MINLP_OptResult.theta_result(knotIdx));-sin(MINLP_OptResult.theta_result(knotIdx)),cos(MINLP_OptResult.theta_result(knotIdx))]*([MINLP_OptResult.PHx_result(knotIdx);MINLP_OptResult.PHy_result(knotIdx)] - [MINLP_OptResult.x_result(knotIdx);MINLP_OptResult.y_result(knotIdx)]);
+        MINLP_OptResult.PHx_result_Robot(knotIdx) = HindLeg_Position_Temp(1);
+        MINLP_OptResult.PHy_result_Robot(knotIdx) = HindLeg_Position_Temp(2);
+        
+    end
+    %   Feet Velocities
+    for knotIdx = 1:length(MINLP_OptResult.theta_result)-1      
+        %Front Leg Velocity
+        FrontLeg_Velo_Temp = [cos(MINLP_OptResult.theta_result(knotIdx)),-sin(MINLP_OptResult.theta_result(knotIdx));sin(MINLP_OptResult.theta_result(knotIdx)),cos(MINLP_OptResult.theta_result(knotIdx))]*([MINLP_OptResult.PFxdot_result(knotIdx);MINLP_OptResult.PFydot_result(knotIdx)] - [MINLP_OptResult.xdot_result(knotIdx);MINLP_OptResult.ydot_result(knotIdx)]);
+        MINLP_OptResult.PFxdot_result_Robot(knotIdx) = FrontLeg_Velo_Temp(1);
+        MINLP_OptResult.PFydot_result_Robot(knotIdx) = FrontLeg_Velo_Temp(2);
+        
+        %Hind Leg Velocity
+        HindLeg_Velo_Temp = [cos(MINLP_OptResult.theta_result(knotIdx)),-sin(MINLP_OptResult.theta_result(knotIdx));sin(MINLP_OptResult.theta_result(knotIdx)),cos(MINLP_OptResult.theta_result(knotIdx))]*([MINLP_OptResult.PHxdot_result(knotIdx);MINLP_OptResult.PHydot_result(knotIdx)] - [MINLP_OptResult.xdot_result(knotIdx);MINLP_OptResult.ydot_result(knotIdx)]);
+        MINLP_OptResult.PHxdot_result_Robot(knotIdx) = HindLeg_Velo_Temp(1);
+        MINLP_OptResult.PHydot_result_Robot(knotIdx) = HindLeg_Velo_Temp(2);       
+    end
+    
+    %Build the trajectories
     Trajectory_MINLP  = [MINLP_OptResult.x_result;      MINLP_OptResult.y_result;        MINLP_OptResult.theta_result;...
-                     MINLP_OptResult.xdot_result;   MINLP_OptResult.ydot_result;     MINLP_OptResult.thetadot_result;...
-                     MINLP_OptResult.PFx_result;    MINLP_OptResult.PFy_result;      MINLP_OptResult.PFxdot_result;       MINLP_OptResult.PFydot_result;...
-                     MINLP_OptResult.PHx_result;    MINLP_OptResult.PHy_result;      MINLP_OptResult.PHxdot_result;       MINLP_OptResult.PHydot_result;...
-                     MINLP_OptResult.FFx_result;    MINLP_OptResult.FFy_result;...
-                     MINLP_OptResult.FHx_result;    MINLP_OptResult.FHy_result;...
-                     MINLP_OptResult.PhaseSwitchingTime]';
+                         MINLP_OptResult.xdot_result;   MINLP_OptResult.ydot_result;     MINLP_OptResult.thetadot_result;...
+                         MINLP_OptResult.PFx_result;    MINLP_OptResult.PFy_result;      MINLP_OptResult.PFxdot_result;       MINLP_OptResult.PFydot_result;...
+                         MINLP_OptResult.PHx_result;    MINLP_OptResult.PHy_result;      MINLP_OptResult.PHxdot_result;       MINLP_OptResult.PHydot_result;...
+                         MINLP_OptResult.FFx_result;    MINLP_OptResult.FFy_result;...
+                         MINLP_OptResult.FHx_result;    MINLP_OptResult.FHy_result;...
+                         MINLP_OptResult.PhaseSwitchingTime]';
    MINLP_Result_DataBase{loopIdx} = MINLP_OptResult;
    Trajectories(loopIdx,:) = Trajectory_MINLP';
 end
