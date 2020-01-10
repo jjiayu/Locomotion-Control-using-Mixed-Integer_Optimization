cb576cf291a11feff63a10c251b0690f78d347ac
Jiayi Wang <jjiayu889@gmail.com>
2020-01-10 17:01:44 +0000

    Update Gait Library Generation using parallel computing

--- 3D_Quadruped/GaitEnumeration/GaitEnumeration.m
+++ 3D_Quadruped/GaitEnumeration/GaitEnumeration.m
@@ -9,35 +9,76 @@ end
 
 GaitSavingNum = 1;
 
-for GaitDecCode = 0:(2^4)^8-1 %GaitDecCode: Gait Number Encoded using Decimals
+parfor i = 0:255
+    %Pre-allocate Saving Locations
+    GaitMatrixResult = zeros((2^4)^6,32);
     
-    if mod(GaitDecCode,100000) == 0
-        disp([num2str(GaitDecCode/(2^4)^8*100),'%']);
-    end
+    GaitSavingNum = 1; 
+    for GaitCode = 1:(2^4)^6
+        GaitDecCode = i*(2^4)^6 + GaitCode - 1;
+        
+        %Convert into Binary
+        GaitBinCode = dec2bin(GaitDecCode,32) - '0';
+        %Make Gait Matrix Format
+        Gait = reshape(GaitBinCode, 4, 8);
     
-    if mod(GaitDecCode,10000000) == 0
-        clc
-    end
+        %Check if the gait is a periodical one
+        FlagCnt = 0; %Count how many limbs meet the periodical requirement
     
-    %Convert into Binary
-    GaitBinCode = dec2bin(GaitDecCode,32) - '0';
-    %Make Gait Matrix Format
-    Gait = reshape(GaitBinCode, 4, 8);
+        for limbNum = 1:4
+            GaitLimb = Gait(limbNum,:); %Gait for a limb
+            ContactChangeTimes = sum(abs(diff(GaitLimb)));
+            if ContactChangeTimes >=1 && ContactChangeTimes <= 2
+                FlagCnt = FlagCnt + 1;
+            end
+        end
     
-    %Check if the gait is a periodical one
-    FlagCnt = 0; %Count how many limbs meet the periodical requirement
-    
-    for limbNum = 1:4
-        GaitLimb = Gait(limbNum,:); %Gait for a limb
-        ContactChangeTimes = sum(abs(diff(GaitLimb)));
-        if ContactChangeTimes >=1 && ContactChangeTimes <= 2
-            FlagCnt = FlagCnt + 1;
+        if FlagCnt == 4 %Meet Periodical Gait Criteria, save
+            GaitMatrixResult(GaitSavingNum,:) = GaitBinCode;
+            GaitSavingNum = GaitSavingNum + 1; %Counter + 1
         end
     end
     
-    if FlagCnt == 4 %Meet Periodical Gait Criteria, save
-        save(['GaitLibrary/PeriodicalGait_',num2str(GaitSavingNum),'.mat'],'Gait');
-        GaitSavingNum = GaitSavingNum + 1; %Counter + 1
-    end
+    GaitMatrixResult = GaitMatrixResult(1:GaitSavingNum-1,:);
+    parsave(i,gait_matrix_result);
     
 end
+
+function parsave(process_number, gait_matrix_result)
+save(['GaitLibrary/Slice_',num2str(process_number)], 'gait_matrix_result');
+end
+
+% 
+% 
+% parfor GaitDecCode = 0:(2^4)^8-1 %GaitDecCode: Gait Number Encoded using Decimals
+%     
+%     if mod(GaitDecCode,100000) == 0
+%         disp([num2str(GaitDecCode/(2^4)^8*100),'%']);
+%     end
+%     
+%     if mod(GaitDecCode,10000000) == 0
+%         clc
+%     end
+%     
+%     %Convert into Binary
+%     GaitBinCode = dec2bin(GaitDecCode,32) - '0';
+%     %Make Gait Matrix Format
+%     Gait = reshape(GaitBinCode, 4, 8);
+%     
+%     %Check if the gait is a periodical one
+%     FlagCnt = 0; %Count how many limbs meet the periodical requirement
+%     
+%     for limbNum = 1:4
+%         GaitLimb = Gait(limbNum,:); %Gait for a limb
+%         ContactChangeTimes = sum(abs(diff(GaitLimb)));
+%         if ContactChangeTimes >=1 && ContactChangeTimes <= 2
+%             FlagCnt = FlagCnt + 1;
+%         end
+%     end
+%     
+%     if FlagCnt == 4 %Meet Periodical Gait Criteria, save
+%         save(['GaitLibrary/PeriodicalGait_',num2str(GaitSavingNum),'.mat'],'Gait');
+%         GaitSavingNum = GaitSavingNum + 1; %Counter + 1
+%     end
+%     
+% end
