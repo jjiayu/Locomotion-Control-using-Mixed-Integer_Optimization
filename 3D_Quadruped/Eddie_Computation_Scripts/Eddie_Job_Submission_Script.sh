745a8a26b0c302d0aa770824df69b6095535e9c9
jiayu <jjiayu889@gmail.com>
2020-02-02 19:23:42 +0000

    Update Eddie Script

--- 3D_Quadruped/Eddie_Computation_Scripts/Eddie_Job_Submission_Script.sh
+++ 3D_Quadruped/Eddie_Computation_Scripts/Eddie_Job_Submission_Script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N Qudaruped_Gait_Discovery              
+#$ -N Quadruped_Gait_Discovery              
 #$ -cwd                  
 #$ -l h_rt=00:05:00 
 #$ -l h_vmem=4G
@@ -11,7 +11,7 @@
 #  memory limit of 1 Gbyte: -l h_vmem
 # Initialise the environment modules
 . /etc/profile.d/modules.sh
- 
+
 # Load Python
 module load matlab/R2018a
 
